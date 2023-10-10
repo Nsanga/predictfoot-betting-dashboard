640570f26bd8ebc3513e2c4488517d1ac9a99491
Nsanga Mercure Mekinda <mercuremekinda@gmail.com>
2023-10-10 16:20:50 +0100

    add link online

--- src/urlLoader.js
+++ src/urlLoader.js
@@ -1,5 +1,5 @@
 const localURL = "http://localhost:5000";
-const liveURL = "";
+const liveURL = "http://16.171.175.191:6000";
 
 module.exports = {
     url:process.env.NODE_ENV ===  'production' ? liveURL : localURL
